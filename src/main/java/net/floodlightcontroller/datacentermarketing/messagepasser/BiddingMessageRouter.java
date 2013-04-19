bf14196cf1f5917ec3fe23f9590061ee4e4996b5
ruizhou <rui_zhou@brown.edu>
2013-04-18 20:57:35 -0400

    json stuff

--- src/main/java/net/floodlightcontroller/datacentermarketing/messagepasser/BiddingMessageRouter.java
+++ src/main/java/net/floodlightcontroller/datacentermarketing/messagepasser/BiddingMessageRouter.java
@@ -7,27 +7,30 @@ import org.restlet.routing.Template;
 
 import net.floodlightcontroller.restserver.RestletRoutable;
 
-public class BiddingMessageRouter implements RestletRoutable{
+public class BiddingMessageRouter implements RestletRoutable
+{
 
-	@Override
-	public Restlet getRestlet(Context context) {
-		// TODO Auto-generated method stub
-		Router router = new Router(context);
-		router.attach("/result/{BidderID}", BidResultResource.class);
-		//router.attach("/request/json/", BidRequestResource.class).setMatchingMode(Template.MODE_STARTS_WITH);
-		router.attach("/request/{BidderID}", BidRequestResource.class);
-		router.attach("/result/", BidResultResource.class);
-		router.attach("/time/", TimeResource.class);
-		//router.at
-		return router;
-	}
+    @Override
+    public Restlet getRestlet(Context context)
+    {
+	// TODO Auto-generated method stub
+	Router router = new Router(context);
+	router.attach("/result/{BidderID}", BidResultResource.class);
+	// router.attach("/request/json/",
+	// BidRequestResource.class).setMatchingMode(Template.MODE_STARTS_WITH);
+	router.attach("/request/{BidderID}", BidRequestResource.class);
+	router.attach("/result/", BidResultResource.class);
+	router.attach("/time/", TimeResource.class);
+	router.attach("/latency/", LatencyTestResource.class);
+	// router.at
+	return router;
+    }
 
-	
-	@Override
-	public String basePath() {
-		// TODO Auto-generated method stub
-		return "/marketing";
-	}
-	
-	
+    @Override
+    public String basePath()
+    {
+	// TODO Auto-generated method stub
+	return "/marketing";
+    }
+
 }
