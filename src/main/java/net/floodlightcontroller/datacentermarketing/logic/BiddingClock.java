78185b643ef88fe054af41a0b2fc9b7a8c6bbb9c
Shu <shu@shu-GE70-0NC.(none)>
2013-04-30 15:39:44 -0400

    struggling

--- src/main/java/net/floodlightcontroller/datacentermarketing/logic/BiddingClock.java
+++ src/main/java/net/floodlightcontroller/datacentermarketing/logic/BiddingClock.java
@@ -10,100 +10,103 @@ import javax.swing.Timer;
 import net.floodlightcontroller.datacentermarketing.messagepasser.ClockJSONSerializer;
 
 import org.codehaus.jackson.map.annotate.JsonSerialize;
-@JsonSerialize(using=ClockJSONSerializer.class)
-public class BiddingClock implements Runnable{
-	//performs as a thread to take care of time
-	//send hint to auctioneer when the auction cycle begins/ends
-	
-	//Singleton
+
+@JsonSerialize(using = ClockJSONSerializer.class)
+public class BiddingClock implements Runnable {
+	// performs as a thread to take care of time
+	// send hint to auctioneer when the auction cycle begins/ends
+
+	// Singleton
 	ActionListener roundCleaner = new ActionListener() {
-	     public void actionPerformed(ActionEvent evt) {
-	    	 //pause the ticker when computing the allocations
-	    	 tickTimer.stop();
-	    	 //time is up
-	    	 Auctioneer.getInstance().setBusy();
-	    	 
-	    	 try {
-	    		 Auctioneer.getInstance().computeAllocation();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (ExecutionException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-	    	 
-	    	 //broadcast result to all participants
-	    	 try {
-				Auctioneer.getInstance().pushResults();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (ExecutionException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-	    	 Auctioneer.getInstance().setNotBusy();
-	    	 PriceAdjuster.getInstance().adjustPrice(); 	 
-	    	 Auctioneer.getInstance().clearRound();
-	    	 //a new round
-	    	 Auctioneer.getInstance().round ++ ;
-	    	 tickTimer.start();
-	     }
-	 };
-	 
-	 ActionListener ticker = new ActionListener() {
-		
+		public void actionPerformed(ActionEvent evt) {
+			// pause the ticker when computing the allocations
+			// time is up
+
+		}
+	};
+
+	ActionListener ticker = new ActionListener() {
+
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			synchronized(this){
-				currentTime ++;
+			synchronized (this) {
+				currentTime++;
+			}
+			if (currentTime % roundTime == 0) {
+				Auctioneer.getInstance().setBusy();
+
+				try {
+					Auctioneer.getInstance().computeAllocation();
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (InterruptedException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (ExecutionException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+
+				// broadcast result to all participants
+				try {
+					Auctioneer.getInstance().pushResults();
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (InterruptedException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (ExecutionException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				Auctioneer.getInstance().setNotBusy();
+				PriceAdjuster.getInstance().adjustPrice();
+				Auctioneer.getInstance().clearRound();
+				// a new round
+				Auctioneer.getInstance().round++;
 			}
 		}
-		
-	 };
-	  
-	private BiddingClock(){
+
+	};
+
+	private BiddingClock() {
 		currentTime = 0;
-		biddingTimer = new Timer(roundTime, roundCleaner);
-		biddingTimer.start();
+		// biddingTimer = new Timer(roundTime, roundCleaner);
+		// biddingTimer.start();
 		tickTimer = new Timer(tick, ticker);
 		tickTimer.start();
-		
+
 	}
-	
+
 	private static BiddingClock _instance = null;
-	public static BiddingClock getInstance(){
-		if(_instance == null)
+
+	public static BiddingClock getInstance() {
+		if (_instance == null)
 			_instance = new BiddingClock();
 		return _instance;
 	}
 
-	//in milliseconds
-	int roundTime= 20000;
+	// in milliseconds
+	int roundTime = 200;
 	int tick = 100;
 	private Timer biddingTimer;
 	private Timer tickTimer;
-	
+
 	long currentTime;
-	
-	public long getCurrentTime(){
-		synchronized (this){
+
+	public long getCurrentTime() {
+		synchronized (this) {
 			return currentTime;
 		}
 	}
-	
+
 	@Override
-	//send the auctioneer signals
-	//when a bidding round begins/ends ???
+	// send the auctioneer signals
+	// when a bidding round begins/ends ???
 	public void run() {
 		// TODO Auto-generated method stub
 	}
-	
+
 }
